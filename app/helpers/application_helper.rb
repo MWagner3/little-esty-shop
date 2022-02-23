d61addd2764a153f510f609f2329bc574711ebf4
Michael Hicks <hicks.michael04@gmail.com>
2022-02-23 08:45:24 -0700

    Add custom css and bootstrap sprockets

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  def full_title(page_title = '') # Method def, optional argument
+  base_title = "The Little Esty Shop" # variable assignment
+  if page_title.empty? # Boolean test
+    base_title# implicit return
+  else
+    page_title + " | " + base_title # string concatenation
+  end
+end
+
 end
