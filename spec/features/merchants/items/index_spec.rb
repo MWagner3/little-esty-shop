e58bda79e94101c5e05c1b9f6f8cd0c417aa4b9f
Sam Iavri <sammi80134@gmail.com>
2022-02-27 13:36:50 -0700

    Test: User Story 29: top 5 items by revenue

--- spec/features/merchants/items/index_spec.rb
+++ spec/features/merchants/items/index_spec.rb
@@ -91,4 +91,165 @@ RSpec.describe 'Merchant Items Index page' do
       expect(page).to have_content('Chimpanzee Cheese')
     end
   end
+
+  describe '#User story #29' do
+    it 'can show the top 5 items based off revenue ' do
+      customer1 = Customer.create!(first_name: 'Jeremy', last_name: 'Fisher')
+      merchant1 = Merchant.create!(name: 'Primate Privleges')
+      item_1 = merchant1.items.create!(name: 'Monkey Paw', description: 'A furry mystery', unit_price: 3)
+      item_2 = merchant1.items.create!(name: 'Gorilla Grip Glue', description: 'A sticky mystery', unit_price: 7)
+      item_3 = merchant1.items.create!(name: 'Lemure lip gloss', description: 'A furry mystery', unit_price: 3)
+      item_4 = merchant1.items.create!(name: 'Chimp cheese', description: 'A sticky mystery', unit_price: 33)
+      item_5 = merchant1.items.create!(name: 'Gorilla Tape', description: 'A furry mystery', unit_price: 3)
+      item_6 = merchant1.items.create!(name: 'Orangutang Tang', description: 'A sticky mystery', unit_price: 33)
+
+      invoice_6 = Invoice.create!(customer_id: customer1.id, status: 2)
+      invoice_7 = Invoice.create!(customer_id: customer1.id, status: 2)
+      invoice_8 = Invoice.create!(customer_id: customer1.id, status: 2)
+      invoice_9 = Invoice.create!(customer_id: customer1.id, status: 2)
+      invoice_10 = Invoice.create!(customer_id: customer1.id, status: 2)
+      invoice_11 = Invoice.create!(customer_id: customer1.id, status: 2)
+
+      transaction_1 = Transaction.create!(invoice_id: invoice_6.id, result: 'success', credit_card_number: '123456789',
+                                          credit_card_expiration_date: '1/2/99')
+      transaction_2 = Transaction.create!(invoice_id: invoice_7.id, result: 'success', credit_card_number: '123456789',
+                                          credit_card_expiration_date: '1/2/99')
+      transaction_3 = Transaction.create!(invoice_id: invoice_8.id, result: 'success', credit_card_number: '123456789',
+                                          credit_card_expiration_date: '1/2/99')
+      transaction_4 = Transaction.create!(invoice_id: invoice_9.id, result: 'success', credit_card_number: '123456789',
+                                          credit_card_expiration_date: '1/2/99')
+      transaction_5 = Transaction.create!(invoice_id: invoice_10.id, result: 'success', credit_card_number: '123456789',
+                                          credit_card_expiration_date: '1/2/99')
+      transaction_6 = Transaction.create!(invoice_id: invoice_11.id, result: 'success', credit_card_number: '123456789',
+                                          credit_card_expiration_date: '1/2/99')
+
+      invoice_item_6 = InvoiceItem.create!(item_id: item_1.id, invoice_id: invoice_6.id, status: 2, quantity: 1,
+                                           unit_price: 50)
+      invoice_item_7 = InvoiceItem.create!(item_id: item_2.id, invoice_id: invoice_7.id, status: 1, quantity: 1,
+                                           unit_price: 45)
+      invoice_item_8 = InvoiceItem.create!(item_id: item_3.id, invoice_id: invoice_8.id, status: 1, quantity: 1,
+                                           unit_price: 40)
+      invoice_item_9 = InvoiceItem.create!(item_id: item_4.id, invoice_id: invoice_9.id, status: 1, quantity: 1,
+                                           unit_price: 35)
+      invoice_item_10 = InvoiceItem.create!(item_id: item_5.id, invoice_id: invoice_10.id, status: 1, quantity: 1,
+                                            unit_price: 30)
+      invoice_item_11 = InvoiceItem.create!(item_id: item_6.id, invoice_id: invoice_11.id, status: 1, quantity: 1,
+                                            unit_price: 5)
+
+      visit "/merchants/#{merchant1.id}/items"
+      within '.top_five_items' do
+        expect(page).to have_link(item_1.name.titleize)
+        expect(page).to have_link(item_2.name.titleize)
+        expect(page).to have_link(item_3.name.titleize)
+        expect(page).to have_link(item_4.name.titleize)
+        expect(page).to have_link(item_5.name.titleize)
+        expect(page).to_not have_content(item_6.name)
+        expect(item_1.name.titleize).to appear_before(item_2.name.titleize)
+        expect(item_2.name.titleize).to appear_before(item_3.name.titleize)
+        expect(item_3.name.titleize).to appear_before(item_4.name.titleize)
+        expect(item_4.name.titleize).to appear_before(item_5.name.titleize)
+      end
+    end
+
+    it 'shows the top 5 items total sales' do
+      customer1 = Customer.create!(first_name: 'Jeremy', last_name: 'Fisher')
+      merchant1 = Merchant.create!(name: 'Primate Privleges')
+      item_1 = merchant1.items.create!(name: 'Monkey Paw', description: 'A furry mystery', unit_price: 3)
+      item_2 = merchant1.items.create!(name: 'Gorilla Grip Glue', description: 'A sticky mystery', unit_price: 7)
+      item_3 = merchant1.items.create!(name: 'Lemure lip gloss', description: 'A furry mystery', unit_price: 3)
+      item_4 = merchant1.items.create!(name: 'Chimp cheese', description: 'A sticky mystery', unit_price: 33)
+      item_5 = merchant1.items.create!(name: 'Gorilla Tape', description: 'A furry mystery', unit_price: 3)
+      item_6 = merchant1.items.create!(name: 'Orangutang Tang', description: 'A sticky mystery', unit_price: 33)
+
+      invoice_6 = Invoice.create!(customer_id: customer1.id, status: 2)
+      invoice_7 = Invoice.create!(customer_id: customer1.id, status: 2)
+      invoice_8 = Invoice.create!(customer_id: customer1.id, status: 2)
+      invoice_9 = Invoice.create!(customer_id: customer1.id, status: 2)
+      invoice_10 = Invoice.create!(customer_id: customer1.id, status: 2)
+      invoice_11 = Invoice.create!(customer_id: customer1.id, status: 2)
+
+      transaction_1 = Transaction.create!(invoice_id: invoice_6.id, result: 'success', credit_card_number: '123456789',
+                                          credit_card_expiration_date: '1/2/99')
+      transaction_2 = Transaction.create!(invoice_id: invoice_7.id, result: 'success', credit_card_number: '123456789',
+                                          credit_card_expiration_date: '1/2/99')
+      transaction_3 = Transaction.create!(invoice_id: invoice_8.id, result: 'success', credit_card_number: '123456789',
+                                          credit_card_expiration_date: '1/2/99')
+      transaction_4 = Transaction.create!(invoice_id: invoice_9.id, result: 'success', credit_card_number: '123456789',
+                                          credit_card_expiration_date: '1/2/99')
+      transaction_5 = Transaction.create!(invoice_id: invoice_10.id, result: 'success', credit_card_number: '123456789',
+                                          credit_card_expiration_date: '1/2/99')
+      transaction_6 = Transaction.create!(invoice_id: invoice_11.id, result: 'success', credit_card_number: '123456789',
+                                          credit_card_expiration_date: '1/2/99')
+
+      invoice_item_6 = InvoiceItem.create!(item_id: item_1.id, invoice_id: invoice_6.id, status: 2, quantity: 1,
+                                           unit_price: 50)
+      invoice_item_7 = InvoiceItem.create!(item_id: item_2.id, invoice_id: invoice_7.id, status: 1, quantity: 1,
+                                           unit_price: 45)
+      invoice_item_8 = InvoiceItem.create!(item_id: item_3.id, invoice_id: invoice_8.id, status: 1, quantity: 1,
+                                           unit_price: 40)
+      invoice_item_9 = InvoiceItem.create!(item_id: item_4.id, invoice_id: invoice_9.id, status: 1, quantity: 1,
+                                           unit_price: 35)
+      invoice_item_10 = InvoiceItem.create!(item_id: item_5.id, invoice_id: invoice_10.id, status: 1, quantity: 1,
+                                            unit_price: 30)
+      invoice_item_11 = InvoiceItem.create!(item_id: item_6.id, invoice_id: invoice_11.id, status: 1, quantity: 1,
+                                            unit_price: 5)
+
+      visit "/merchants/#{merchant1.id}/items"
+      within '.top_five_items' do
+        expect(page).to have_content("Item: #{item_1.name}")
+        expect(page).to have_content("Total Sales: #{invoice_item_6.quantity * invoice_item_6.unit_price}")
+      end
+    end
+
+    it 'has the top 5 items as links to their show page' do
+      customer1 = Customer.create!(first_name: 'Jeremy', last_name: 'Fisher')
+      merchant1 = Merchant.create!(name: 'Primate Privleges')
+      item_1 = merchant1.items.create!(name: 'Monkey Paw', description: 'A furry mystery', unit_price: 3)
+      item_2 = merchant1.items.create!(name: 'Gorilla Grip Glue', description: 'A sticky mystery', unit_price: 7)
+      item_3 = merchant1.items.create!(name: 'Lemure lip gloss', description: 'A furry mystery', unit_price: 3)
+      item_4 = merchant1.items.create!(name: 'Chimp cheese', description: 'A sticky mystery', unit_price: 33)
+      item_5 = merchant1.items.create!(name: 'Gorilla Tape', description: 'A furry mystery', unit_price: 3)
+      item_6 = merchant1.items.create!(name: 'Orangutang Tang', description: 'A sticky mystery', unit_price: 33)
+
+      invoice_6 = Invoice.create!(customer_id: customer1.id, status: 2)
+      invoice_7 = Invoice.create!(customer_id: customer1.id, status: 2)
+      invoice_8 = Invoice.create!(customer_id: customer1.id, status: 2)
+      invoice_9 = Invoice.create!(customer_id: customer1.id, status: 2)
+      invoice_10 = Invoice.create!(customer_id: customer1.id, status: 2)
+      invoice_11 = Invoice.create!(customer_id: customer1.id, status: 2)
+
+      transaction_1 = Transaction.create!(invoice_id: invoice_6.id, result: 'success', credit_card_number: '123456789',
+                                          credit_card_expiration_date: '1/2/99')
+      transaction_2 = Transaction.create!(invoice_id: invoice_7.id, result: 'success', credit_card_number: '123456789',
+                                          credit_card_expiration_date: '1/2/99')
+      transaction_3 = Transaction.create!(invoice_id: invoice_8.id, result: 'success', credit_card_number: '123456789',
+                                          credit_card_expiration_date: '1/2/99')
+      transaction_4 = Transaction.create!(invoice_id: invoice_9.id, result: 'success', credit_card_number: '123456789',
+                                          credit_card_expiration_date: '1/2/99')
+      transaction_5 = Transaction.create!(invoice_id: invoice_10.id, result: 'success', credit_card_number: '123456789',
+                                          credit_card_expiration_date: '1/2/99')
+      transaction_6 = Transaction.create!(invoice_id: invoice_11.id, result: 'success', credit_card_number: '123456789',
+                                          credit_card_expiration_date: '1/2/99')
+
+      invoice_item_6 = InvoiceItem.create!(item_id: item_1.id, invoice_id: invoice_6.id, status: 2, quantity: 1,
+                                           unit_price: 50)
+      invoice_item_7 = InvoiceItem.create!(item_id: item_2.id, invoice_id: invoice_7.id, status: 1, quantity: 1,
+                                           unit_price: 45)
+      invoice_item_8 = InvoiceItem.create!(item_id: item_3.id, invoice_id: invoice_8.id, status: 1, quantity: 1,
+                                           unit_price: 40)
+      invoice_item_9 = InvoiceItem.create!(item_id: item_4.id, invoice_id: invoice_9.id, status: 1, quantity: 1,
+                                           unit_price: 35)
+      invoice_item_10 = InvoiceItem.create!(item_id: item_5.id, invoice_id: invoice_10.id, status: 1, quantity: 1,
+                                            unit_price: 30)
+      invoice_item_11 = InvoiceItem.create!(item_id: item_6.id, invoice_id: invoice_11.id, status: 1, quantity: 1,
+                                            unit_price: 5)
+
+      visit "/merchants/#{merchant1.id}/items"
+      within '.top_five_items' do
+        expect(page).to have_content("Item: #{item_1.name}")
+        click_on item_1.name
+      end
+      expect(current_path).to eq("/merchants/#{merchant1.id}/items/#{item_1.id}")
+    end
+  end
 end
