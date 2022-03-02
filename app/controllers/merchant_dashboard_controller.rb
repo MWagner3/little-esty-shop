c801bf1033328fa7fa75575ad1b50c05ddf3b69b
Michael Hicks <hicks.michael04@gmail.com>
2022-03-02 15:37:25 -0700

    temporary fix

---
+++ app/controllers/merchant_dashboard_controller.rb
@@ -0,0 +1,11 @@
+branclass MerchantDashboardController < ApplicationController
+
+  # def index
+  #   @merchant = Merchant.all(params[:merchant_id])
+  # end
+
+  def show
+    @merchant = Merchant.find(params[:id])
+  end
+
+end
