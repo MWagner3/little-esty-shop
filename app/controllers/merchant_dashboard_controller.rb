7889aa94a3d994c2c839e84a7d6eaa6150b293c5
Michael Hicks <hicks.michael04@gmail.com>
2022-03-02 17:23:50 -0700

    Add index to controller

--- app/controllers/merchant_dashboard_controller.rb
+++ app/controllers/merchant_dashboard_controller.rb
@@ -1,11 +1,11 @@
-branclass MerchantDashboardController < ApplicationController
-
-  # def index
-  #   @merchant = Merchant.all(params[:merchant_id])
-  # end
+class MerchantDashboardController < ApplicationController
 
   def show
-    @merchant = Merchant.find(params[:id])
+    @merchant = Merchant.find(params[:merchant_id])
+  end
+  
+  def index
+    @merchant = Merchant.find(params[:merchant_id])
   end
 
 end
